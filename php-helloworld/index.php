fc985bc0c35c9851ff5614f29a2b21865c034dee
Student User <student@workstation.lab.example.com>
2020-09-30 10:46:46 +0000

    error fixed

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
-print "Content is changed\n"
-print "Content is changed\n"
+print "Content is changed\n";
+print "Content is changed\n";
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 ?>
